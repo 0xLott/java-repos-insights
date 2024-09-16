dbeeae78f152fbf0a4af9473f44b5df7670d4008
0xlott <103859841+0xLott@users.noreply.github.com>
2024-09-16 10:42:18 +0000

    Iterative metrics calculation

--- code/main.py
+++ code/main.py
@@ -4,17 +4,20 @@ from stats import calculate_metrics
 from ck_runner import run_ck
 
 batch_size = 20
-repos_quantity = 1000
+repos_amount = 1000
 
 if __name__ == '__main__':
     with open("query/query.gql", "r") as file:
         query = file.read()
 
-    variables = {'num_repos': repos_quantity}
+    variables = {'num_repos': repos_amount}
 
     print('''
     1- Fetch repository data and clone repos 
     2- Get CK metrics
+
+    ⚠ To run Option 2, you need to have both the query results and cloned repositories 
+    stored in your project. Execute Option 1 first to obtain these necessary files.
     ''')
     
     option = input("Action: ")
@@ -24,7 +27,4 @@ if __name__ == '__main__':
             start_clonage('query/results/results-0.csv')
         case "2":
             run_ck()
-            # calculate_metrics()
-            
-
-
+            calculate_metrics()
--- code/stats.py
+++ code/stats.py
@@ -7,6 +7,45 @@ import os
 import pandas as pd
 import numpy as np
 
+def calculate_metrics():
+    root_path = 'ck/output'
+
+    for dirpath, dirs, files in os.walk(root_path):
+        for dir in dirs:
+            # Gather this repository's data from results.csv
+            repos_data = pd.read_csv('query/results/results-0.csv')   
+            row = repos_data[repos_data["name"] == dir]
+
+            # Gather this repository's data from ck outputs
+            classes_data = pd.read_csv('ck/output/' + dir +  '/class.csv')
+            methods_data = pd.read_csv('ck/output/' + dir + '/method.csv')
+
+            cbo = cbo_from_csv(classes_data)
+            dit = dit_from_csv(classes_data)
+            lcom = lcom_from_csv(classes_data)
+            loc = loc_from_csv(methods_data)
+            [age, stars, releases] = process_row(row)
+
+            print("\n" + dir + " metrics:")
+            display_metrics(cbo, dit, lcom, loc, age, stars, releases)
+
+def process_row(row):   
+    row = row.iloc[0]
+    age = calculate_age(row['createdAt'])
+    stars = row['stargazerCount']
+    releases = calculate_releases(row['releases'])
+
+    return age, stars, releases
+
+def display_metrics(cbo, dit, lcom, loc, age, stars, releases):
+    print(f"\t CBO: {cbo}")
+    print(f"\t DIT: {dit}")
+    print(f"\t LCOM*: {lcom}")
+    print(f"\t # of releases: {releases}")
+    print(f"\t LOC: {loc}")
+    print(f"\t # of stars: {stars}")
+    print(f"\t Age (in days): {age}")
+
 # CBO: Coupling between objects
 def cbo_from_csv(data):
     df = data
@@ -28,24 +67,13 @@ def lcom_from_csv(data):
     total_lcom = pd.to_numeric(df['lcom*'], errors='coerce').mean()
     return total_lcom
 
-# Number of stars
-def star_count_from_csv(data):
-    df = data
-    total_stars_mean = df['stargazerCount'].mean()
-
-    return total_stars_mean
-
 # Number of releases
-def releases_from_csv(data):
-    total_releases = []
-
-    for row in data['releases']:
-        release_data = ast.literal_eval(row)
-        total_releases.append(release_data['totalCount'])
-
-    total_releases_mean = np.mean(total_releases)
-
-    return total_releases_mean
+def calculate_releases(data):
+    try:
+        releases_dict = eval(data)
+        return int(releases_dict.get('totalCount', 0))
+    except (SyntaxError, ValueError, AttributeError):
+        return 0
 
 # LOC: Lines of Code
 def loc_from_csv(data):
@@ -55,30 +83,8 @@ def loc_from_csv(data):
     return total_loc
 
 # Age
-def age_from_csv(data):
-    repo_age = []
-    today = datetime.today().date()
-
-    for row in data['createdAt']:
-        date_obj = datetime.fromisoformat(row.replace('Z', '+00:00')).date()
-        age_in_days = today - date_obj
-        repo_age.append(age_in_days.days)
-
-    created_at_median = np.median(repo_age)
-    
-    return created_at_median
-
-
-
-def calculate_metrics():
-    classes_data = pd.read_csv('ck/output/class.csv')
-    methods_data = pd.read_csv('ck/output/method.csv')
-    repos_data = pd.read_csv('query/results/results-0.csv')    
-
-    # print("CBO:", cbo_from_csv(classes_data))
-    # print("DIT:", dit_from_csv(classes_data))
-    # print("LCOM*:", lcom_from_csv(classes_data))
-    # print("# of releases:", releases_from_csv(repos_data))
-    # print("LOC:", loc_from_csv(methods_data))
-    # print("# of stars:", star_count_from_csv(repos_data))
-    # print("Age (in days):", age_from_csv(repos_data))
+def calculate_age(data):
+    created_date = datetime.fromisoformat(data.replace('Z', '+00:00'))
+    current_date = datetime.now(timezone.utc)
+    age = (current_date - created_date).days
+    return age
