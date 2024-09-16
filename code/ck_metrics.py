96a941defd0a65051daf9711136159a09132a75a
0xlott <103859841+0xLott@users.noreply.github.com>
2024-09-16 03:01:53 +0000

    Pandas refactor and metrics calculation

--- code/ck_metrics.py
+++ code/ck_metrics.py
@@ -2,48 +2,37 @@ import csv
 import math
 import ast
 import pandas as pd
+import numpy as np
 
 # CBO: Coupling between objects
 def cbo_from_csv(file_path):
-    total_cbo = 0
-    with open(file_path, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            try:
-                cbo_value = float(row['cbo'])
-                total_cbo += cbo_value
-            except ValueError:
-                continue
+    df = pd.read_csv(file_path)
+    
+    total_cbo = pd.to_numeric(df['cbo'], errors='coerce').sum()
     return total_cbo
 
 # DIT: Depth of Inheritance Tree
 def dit_from_csv(file_path):
-    total_dit = 0
-    with open(file_path, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            try:
-                dit_value = float(row['dit'])
-                total_dit += dit_value
-            except ValueError:
-                continue
+    df = pd.read_csv(file_path)
+    
+    total_dit = pd.to_numeric(df['dit'], errors='coerce').sum()
     return total_dit
 
 # LCOM*: Lack of Cohesion of Methods
 def lcom_from_csv(file_path):
-    total_lcom = 0
-    with open(file_path, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            try:
-                lcom_value = float(row['lcom*'])
-                if not math.isnan(lcom_value):
-                    total_lcom += lcom_value
-            except ValueError:
-                continue
+    df = pd.read_csv(file_path)
+    
+    total_lcom = pd.to_numeric(df['lcom*'], errors='coerce').sum()
     return total_lcom
 
-# Releases
+# Number of star
+def star_count_from_csv(data):
+    df = data
+    total_stars_mean = df['stargazerCount'].mean()
+
+    return total_stars_mean
+
+# Number of releases
 def releases_from_csv(data):
     total_releases = []
 
@@ -52,17 +41,6 @@ def releases_from_csv(data):
         total_releases.append(release_data['totalCount'])
 
     total_releases_mean = np.mean(total_releases)
-
-    plt.clf()
-
-    plt.hist(total_releases, bins=30, color='salmon', edgecolor='black')
-    plt.axvline(total_releases_mean, color='red', linestyle='dashed', linewidth=1)
-    plt.title('Distribuição do Total de Releases')
-    plt.xlabel('Número de Releases')
-    plt.ylabel('Frequência')
-
-    plt.savefig('./plots/rq_03.jpg')
-    plt.show()
 
     return total_releases_mean
 
@@ -78,3 +56,4 @@ def metrics():
     print("DIT:", dit_from_csv(classes_file_path))
     print("LCOM*:", lcom_from_csv(classes_file_path))
     print("# of releases:", releases_from_csv(repos_data))
+    print("# of stars:", star_count_from_csv(repos_data))
